c7c64c27531f5da51a628f59713f56b6fbacdbda
João Nobre <joaoanobre@gmail.com>
2022-12-09 18:25:25 +0100

    Add solution to Day 1 problems

--- day01/day01.go
+++ day01/day01.go
@@ -1,8 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+
+	AOCUTILS "github.com/jnobre/advent-of-code-2022"
+)
+
 func main() {
 	lines := AOCUTILS.ReadLines("input.txt")
 	groups := make([]int, 0)
 	curr := 0
-
+	for _, el := range lines {
+		num, err := strconv.Atoi(el)
+		if err != nil {
+			groups = append(groups, curr)
+			curr = 0
+		} else {
+			curr += num
+		}
+	}
+	groups = append(groups, curr)
+	groups = AOCUTILS.SortDescending(groups)
+	fmt.Println(groups[0])
+	fmt.Println(AOCUTILS.Sum(groups[:3]))
 }
